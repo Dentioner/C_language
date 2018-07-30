5baceeb2de275ba14bfb014192c44be599379078
Dentioner <37292492+Dentioner@users.noreply.github.com>
2018-07-30 11:49:22 +0800

    %#o与%d相比，还能显示数字的类型

---
+++ 3.3/3.3/3.3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x = 100;
+
+	printf("dec = %d; octal = %o; hex = %x\n", x, x, x);
+	printf("dec = %d; octal = %#o; hex = %#x\n",x ,x ,x);
+
+	system("pause");
+	return 0;
+}
