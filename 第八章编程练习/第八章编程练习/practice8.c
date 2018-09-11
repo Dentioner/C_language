80cee6cb22af076845df27d491a51da94acdd42b
Dentioner <37292492+Dentioner@users.noreply.github.com>
2018-09-11 21:58:32 +0800

    until p241 T2
    安装了一个VSCode做备用

---
+++ 第八章编程练习/第八章编程练习/practice8.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{//T1
+	/*
+	char ch = 0;
+	int number = 0;
+	while ((ch = getchar()) != 0)
+	{
+		if (ch == '\n')
+			continue;
+		if (ch == EOF)
+			break;
+		number++;
+	}
+	printf("总共有%d个字符\n", number);
+	*/
+	
+	printf("Done.\n");
+	return 0;
+}
+
