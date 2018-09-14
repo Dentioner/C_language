138b118e703d41dbaa0c59e2aadd63ee4966a73f
Dentioner <37292492+Dentioner@users.noreply.github.com>
2018-09-14 21:56:46 +0800

    until T9

--- 第九章编程练习/第九章编程练习/practice9.c
+++ 第九章编程练习/第九章编程练习/practice9.c
@@ -2,7 +2,9 @@
 #include<stdbool.h>
 double min(double x, double y);
 bool chline(char ch, int i, int j);
-
+void pointer_change(double *x, double *y, double*z);
+int where_is_it(char letter);
+double power(double n, int p);
 int main()
 {	
 	
@@ -65,6 +67,54 @@ int main()
 	printf("yatazze!\n");
 	printf("The char is %c", ch);
 	*/
+	//T6
+	/*
+	double x, y, z;
+	printf("Please input 3 numbers.\n");
+	scanf("%lf %lf %lf", &x, &y, &z);
+	printf("Now the 3 numbers are x=%lf, y=%lf, z=%lf\n", x, y, z);
+	pointer_change(&x, &y, &z);
+	printf("Now the 3 numbers become x=%lf, y=%lf, z=%lf\n", x, y, z);
+*/
+	//T7
+	/*
+	char letter;
+	int loc;
+	printf("Please input a text.\n");
+	while ((letter = getchar()) != EOF)
+	{
+		fflush(stdin);
+		loc = where_is_it(letter);
+		printf("字母在第%d位\n", loc);
+	}
+	*/
+	double n;
+	int p;
+	double consequence;
+	char quit;
+	printf("按q退出，其它任意键开始\n");
+	while ((quit = getchar()) != 'q')
+	{
+		while (getchar() != '\n')
+			continue;
+		
+		printf("请输入底数和指数\n");
+		while ((scanf("%lf %d", &n, &p))!=2)
+		{
+			printf("输入无效，请再输入一次\n");
+			while (getchar() != '\n')
+				continue;
+		}
+
+		consequence = power(n, p);
+		printf("结果是%lf\n", consequence);
+
+		printf("按q退出，其它任意键继续\n");
+		while (getchar() != '\n')
+			continue;
+	}
+	
+	
 	return 0;
 }
 
@@ -88,4 +138,141 @@ bool chline(char ch, int i, int j)
 		no_get_it = false;
 	//putchar(ch);
 	return no_get_it;
+}
+/*
+void pointer_change(double *x, double *y, double*z)
+{
+	double tx, ty, tz;
+	double s, m, l;
+	tx = *x;
+	ty = *y;
+	tz = *z;
+	
+	if (tx > ty)
+	{
+		
+		if (ty > tz)
+		{
+			s = tz;
+			l = tx;
+			m = ty;
+		}
+		else
+		{
+			if (tx > tz)
+			{
+				l = tx;
+				m = tz;
+				s = ty;
+			}
+			else
+			{
+				l = tz;
+				m = tx;
+				s = ty;
+			}
+		}
+	}
+	else
+	{
+		if (ty < tz)
+		{
+			m = ty;
+			l = tz;
+			s = tx;
+		}
+		else
+		{
+			if (tx > tz)
+			{
+				s = tz;
+				m = tx;
+				l = ty;
+			}
+			else
+			{
+				l = ty;
+				m = tz;
+				s = tx;
+			}
+			
+		}
+	}
+	*x = s;
+	*y = m;
+	*z = l;
+	return;
+}*/
+//以上是我自己的排序
+//以下是冒泡排序法
+void pointer_change(double *x, double *y, double*z)
+{
+	double tx, ty, tz;
+	double temp = 0.0;
+	tx = *x;
+	ty = *y;
+	tz = *z;
+	if (tx > ty)
+	{
+		temp = tx;
+		tx = ty;
+		ty = temp;
+	}
+	if (tx > tz)
+	{
+		temp = tx;
+		tx = tz;
+		tz = temp;
+	}
+	if (ty > tz)
+	{
+		temp = ty;
+		ty = tz;
+		tz = temp;
+	}
+	*x = tx;
+	*y = ty;
+	*z = tz;
+
+}
+
+int where_is_it(char letter)
+{
+	char const_letter = 'a';
+	char const_Letter = 'A';
+	int location = 1;
+	while (letter != const_letter && letter != const_Letter)
+	{
+		if (location > 26)
+		{
+			location = -1;
+			break;
+		}
+		location++;
+		const_Letter++;
+		const_letter++;
+	}
+	
+
+	return location;
+}
+
+double power(double n, int p)
+{
+	int index;
+	double consequence =1;
+	if (p == 0)
+	{
+		consequence = 1;
+	}
+	else
+	{
+		for (index = 0; index < p; index++)
+		{
+			consequence *= n;
+		}
+		
+	}
+
+	return consequence;
 }
--- 第九章编程练习范例/第九章编程练习范例/范例.c
+++ 第九章编程练习范例/第九章编程练习范例/范例.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
+/*
 void chline(char *ch, int i, int j)
 {
 	int m = 0;
@@ -49,6 +50,54 @@ void main(void)
 	chline(words, i, j);
 
 	printf("\n");
+
+	return;
+}
+*/
+void rank_three_double(double *number1, double *number2, double *number3)
+{
+	double tmp = 0.0;
+	if (*number1 > *number2)
+	{
+		tmp = *number1;
+		*number1 = *number2;
+		*number2 = tmp;
+	}
+	if (*number1 > *number3)
+	{
+		tmp = *number1;
+		*number1 = *number3;
+		*number3 = tmp;
+	}
+	if (*number2 > *number3)
+	{
+		tmp = *number2;
+		*number2 = *number3;
+		*number3 = tmp;
+	}
+	return;
+}
+void p9_6(void)
+{
+	double number1 = 0.0;
+	double number2 = 0.0;
+	double number3 = 0.0;
+
+	printf("Enter the first double number:");
+	scanf_s("%lf", &number1);
+	getchar();
+
+	printf("Enter the second double number:");
+	scanf_s("%lf", &number2);
+	getchar();
+
+	printf("Enter the third double number:");
+	scanf_s("%lf", &number3);
+	getchar();
+
+	printf("Before rank, the three double number are: %lf, %lf and %lf\n", number1, number2, number3);
+	rank_three_double(&number1, &number2, &number3);
+	printf("After rank, the three double number are: %lf, %lf and %lf\n", number1, number2, number3);
 
 	return;
 }
