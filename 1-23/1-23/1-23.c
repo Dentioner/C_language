01c2937a88f46fdd8523eae755e71bae4226f089
Dentioner <37292492+Dentioner@users.noreply.github.com>
2018-10-05 10:53:32 +0800

    国庆作业修改
    用了状态机的概念

--- 1-23/1-23/1-23.c
+++ 1-23/1-23/1-23.c
@@ -1,80 +1,108 @@
 #include<stdio.h>
 
-
-
-
+void delete_notation(void);
 int main(void)
 {
+	delete_notation();
+	return 0;
+}
+
+void delete_notation(void)
+{
 	int ch;
-	int re_ch = 'a';
+	int state = 0;
 	while ((ch = getchar()) != EOF)
 	{
-		if ((ch == '/')&&(re_ch !='/'))
+		if (state == 0 && ch == '/')
 		{
-			re_ch = ch;
-			
+			state = 1;
 		}
-
-		/*
-		else if ((re_ch == '/') && (ch != '/'))
+		else if (state == 0 && ch == '\"')
 		{
-			putchar(re_ch);
-			putchar(ch);
+			state = 7;
 		}
-		*/
-		else if ((re_ch == '/') && (ch != '*') && (ch != '/'))
+		else if (state == 0 && ch == '\'')
 		{
-			putchar(re_ch);
-			putchar(ch);
+			state = 5;
 		}
-		else if ((ch == '*') && (re_ch != '/'))
+		else if(state == 1 && ch !='/' && ch!='*')
 		{
-			putchar(ch);
-
+			state = 0;
+			putchar('/');
 		}
-		else if ((ch == '/') && (re_ch == '/'))
+		else if (state == 1 && ch == '/')
 		{
-			while ((ch = getchar()) != EOF)
-			{
-				if (ch == '\n')
-				{
-					putchar(ch);
-					re_ch = 'a';
-					break;
-				}
-			}
-
+			state = 4;//state4就是单行注释状态
 		}
-		else if ((re_ch == '/') && (ch == '*'))
+		else if (state == 1 && ch == '*')
 		{
-			while ((ch = getchar()) != EOF)
-			{
-				if (ch == '*')
-				{
-					re_ch = ch;
-				}
-				else if ((ch == '/') && (re_ch == '*'))
-				{
-					while ((getchar() != '\n'))
-						continue;
-					break;
-				}
+			state = 2;//开始多行注释
+		}
+		else if (state == 2 && ch == '*')
+		{
+			state = 3;
+		}
+		else if (state == 2 && ch != '*')
+		{
+			state = 2;
+		}
+		else if (state == 3 && ch == '*')
+		{
+			state = 3;
+		}
+		else if (state == 3 && ch == '/')
+		{
+			state = 0;
+		}
+		else if (state == 3 && ch != '/'&& ch != '*')
+		{
+			state = 2;
+		}
+		else if (state == 4 && ch == '\n')//结束单行注释
+		{
+			state = 0;
+		}
+		else if (state == 4 && ch == '\\')
+		{
+			state = 9;
+		}
+		else if (state == 5 && ch == '\\')
+		{
+			state = 6;
+		}
+		else if (state == 5 && ch == '\'')
+		{
+			state = 0;
+		}
+		else if (state == 6)
+		{
+			state = 5;
+		}
+		else if (state == 7 && ch == '\\')
+		{
+			state = 8;
+		}
+		else if (state == 7 && ch == '\"')
+		{
+			state = 0;
+		}
+		else if (state == 8)
+		{
+			state = 7;
+		}
+		else if (state == 9 && ch == '\\')
+		{
+			state = 9;
+		}
+		else if (state == 9 && ch != '\\')
+		{
+			state = 4;
+		}
 	
-					
-			}
-		}
-		else
+		if ((state == 0 && ch != '/') || (state == 5) || (state == 6) || (state == 7) || (state == 8))
 		{
 			putchar(ch);
 		}
-		
-		if (ch == EOF)
-		{
-			break;
-		}
+	
 	}
-		
-	return 0;
-
-
 }
---
+++ test_for_1-23/Debug/output.c
@@ -0,0 +1,38 @@
+
+
+#          include         <stdio.h>
+#  include  <secure/_stdio.h>
+#include      "/Users/apple/blog/zhanghaiba/KandR/test.h"
+#define CHAR '\'' 
+#  define LESS(i) ( ((i) << 31) / 2 )
+#        define STRING "\"string\"" 
+
+int main(void)
+{
+    int w; 
+    int x;
+    double y; 
+    double z; 
+
+    
+    int idx;
+    if (idx > 3 && idx < 6) idx = idx/100; 
+
+        
+    char a = '/';    
+    char b = '*';    
+    char c = '\'';    
+    char d = '\n';    
+    char e = '\"';    
+    char f = '\\';    
+    char h = '>';    
+    char i = '"';    
+
+    
+    char special0[] = "\"hello, world!\"";
+    char special1[] = "//test";
+    char special2[] = "he\"/*hehe*/";
+    char *special = " \' hi \0\b\t \\\\ \a\e\f\n\r\v wolegequ \\ ";
+    return        0;
+}s
+
