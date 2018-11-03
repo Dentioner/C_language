a26f302c14c092b9d3a41f143a5b529a3d25dc0d
Dentioner <we980127@gmail.com>
2018-11-03 21:14:18 +0800

    a stable version without VCX

--- test2/test2/test2.c
+++ test2/test2/test2.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
-int a;
-a = 0;
+
 int main()
 {
 
-	
-		char* str1 = "HaLLo, world";
-		char* str2 = "Welcome you";
-		char* strp = strpbrk(str1, str2);
-		if (strp != NULL)
-			printf("%s\n", strp);
+	printf("%d", sizeof(unsigned long long));
+		
 		return 0;
 
 }
